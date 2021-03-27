5c8b0f4e31bc2295e387fabdbeafe8d253b8320e
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-03-27 19:08:06 -0300

    Guests RecyclerView

--- guests/app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
+++ guests/app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
@@ -8,25 +8,41 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.R
+import com.example.guests.view.adapter.GuestAdapter
 import com.example.guests.viewmodel.AllGuestsViewModel
+import kotlinx.android.synthetic.main.fragment_all.*
 
 class AllGuestsFragment : Fragment() {
 
     private lateinit var allGuestsViewModel: AllGuestsViewModel
+    private val mAdapter: GuestAdapter = GuestAdapter()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        allGuestsViewModel =
-                ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        allGuestsViewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_all, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        allGuestsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val recycler = root.findViewById<RecyclerView>(R.id.all_guests_view)
+        recycler.layoutManager = LinearLayoutManager(context)
+        recycler.adapter = mAdapter
+
+        observer()
+
+        allGuestsViewModel.load()
+
         return root
+    }
+
+    private fun observer() {
+        //viewLifecycleOwner has the same role for fragments as Context has for Activities
+        allGuestsViewModel.guestList.observe(viewLifecycleOwner, Observer {
+            mAdapter.updateGuests(it)
+        })
     }
 }
